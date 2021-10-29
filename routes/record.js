b44cd974501e65319194388b942421307b1cd812
Novater <cookiesnmilkjk@gmail.com>
2021-10-29 19:35:23 -0400

    add endpoints

--- routes/record.js
+++ routes/record.js
@@ -22,15 +22,61 @@ recordRoutes.route('/record').get((req, res) => {
     });
 });
 
-recordRoutes.route('/post').get((req, res) => {
+recordRoutes.route('/post/:name').get((req, res) => {
+  const { name } = req.params;
   let db_connect = dbo.getDb('leaderboard');
+  let query = { tabname: name }
   db_connect
     .collection('posts')
-    .find({})
+    .find(query)
     .toArray((err, result) => {
       if (err) throw err;
       res.json(result);
     });
-})
+});
+
+recordRoutes.route('/post/delete').post((req, res) => {
+  const id = ObjectId(req.body.id);
+  let db_connect = dbo.getDb('leaderboard');
+
+  let query = { 
+    _id: id 
+  };
+
+  db_connect
+    .collection('posts')
+    .deleteOne(query, (err, result) => {
+      if (err) throw err;
+      console.log(`Document ${id} deleted.`);
+      console.log('result', result);
+      res.json(result);
+    });
+});
+
+recordRoutes.route('/post/update').post((req, res) => {
+  const id = ObjectId(req.body.id);
+  const { title, content } = req.body;
+  let db_connect = dbo.getDb('leaderboard');
+
+  let query = {
+    _id: id
+  };
+  let updatedDoc = {
+    title,
+    content
+  };
+
+  db_connect
+    .collection('posts')
+    .updateOne(query, { $set: updatedDoc })
+    .then(res => {
+      console.log('result', res);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+
+
+});
 
 module.exports = recordRoutes;
